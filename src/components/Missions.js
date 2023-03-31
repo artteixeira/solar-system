4b0413b02c5860619f471bebc070c08f84e02625
Arthur Teixeira <arthursony1@hotmail.com>
2023-03-31 20:03:23 -0300

    requisito 9 e 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionsInfo from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {MissionsInfo.map((element, index) => (
+          <MissionCard
+            key={ index }
+            name={ element.name }
+            year={ element.year }
+            country={ element.country }
+            destination={ element.destination }
+          />
+        ))}
       </div>
     );
   }
