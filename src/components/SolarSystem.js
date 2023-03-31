75a649ab28777d21006a6a89f7e0616e57858bd8
Arthur Teixeira <arthursony1@hotmail.com>
2023-03-31 13:19:18 -0300

    requitiso 5 e 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {Planets.map((element, index) => (
+          <PlanetCard
+            key={ index }
+            planetName={ element.name }
+            planetImage={ element.image }
+          />
+        ))}
       </div>
     );
   }
